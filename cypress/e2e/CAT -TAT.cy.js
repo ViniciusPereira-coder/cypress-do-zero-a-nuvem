a856d544cfaabd72bf14af5f6ce818d18f12976d
Vinicius Pereira <vinicius_moraespereira@hotmail.com>
2025-04-15 00:15:52 -0300

    Update CAT -TAT.cy.js

--- cypress/e2e/CAT -TAT.cy.js
+++ cypress/e2e/CAT -TAT.cy.js
@@ -2,6 +2,14 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.visit('./src/index.html')
 
-
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
+
+
+
+
+
+
+
+  
 })
